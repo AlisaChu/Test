5aec467159ab837d28835c378fb986f3b43ac887
AlisaChu <shendrovska89@gmail.com>
2022-12-04 17:35:19 +0100

    ready1

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -11,6 +11,6 @@ public class ArrayTest {
             int[] arr = {5, 20, 87, 100, 13};
             int expected = 45;
             int output = arrayCal.ArrayCal(arr);
-            Assert.assertEquals(expected, output, 1);
+            Assert.assertEquals(expected, output);
     }
 }
